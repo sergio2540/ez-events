43931a97c3b1081eb924344360b68cece946eb72
Bruno Braga <bruno.mh.braga@gmail.com>
2014-11-16 17:15:57 +0000

    Event title,description,time,, place, checklist (incomplete)

--- gen/com/example/android/effectivenavigation/R.java
+++ gen/com/example/android/effectivenavigation/R.java
@@ -15,6 +15,11 @@ public final class R {
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
+
+         Example customization of dimensions originally defined in res/values/dimens.xml
+         (such as screen margins) for screens with more than 820dp of available width. This
+         would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
+    
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
@@ -35,40 +40,56 @@ public final class R {
         public static final int venus=0x7f02000c;
     }
     public static final class id {
-        public static final int button1=0x7f070004;
-        public static final int content_frame=0x7f070008;
-        public static final int create_account=0x7f070005;
-        public static final int drawer_layout=0x7f070007;
-        public static final int editText1=0x7f07000e;
-        public static final int editText2=0x7f07000d;
-        public static final int image=0x7f07000b;
-        public static final int imageView1=0x7f070006;
-        public static final int left_drawer=0x7f070009;
-        public static final int my_events=0x7f07000c;
-        public static final int pager=0x7f070000;
-        public static final int pager_title_strip=0x7f070001;
-        public static final int password=0x7f070002;
-        public static final int rowTextView=0x7f07000a;
-        public static final int textView1=0x7f07000f;
-        public static final int username=0x7f070003;
+        public static final int action_settings=0x7f080016;
+        public static final int button1=0x7f080002;
+        public static final int content_frame=0x7f080011;
+        public static final int create_account=0x7f08000e;
+        public static final int drawer_layout=0x7f080010;
+        public static final int editText1=0x7f080005;
+        public static final int editText2=0x7f080006;
+        public static final int expandableListView1=0x7f080001;
+        public static final int firstNextButton=0x7f080007;
+        public static final int image=0x7f080014;
+        public static final int imageView1=0x7f08000f;
+        public static final int left_drawer=0x7f080012;
+        public static final int my_events=0x7f080015;
+        public static final int pager=0x7f080003;
+        public static final int pager_title_strip=0x7f080004;
+        public static final int password=0x7f08000c;
+        public static final int rowTextView=0x7f080013;
+        public static final int secondNextButton=0x7f080008;
+        public static final int textView1=0x7f080000;
+        public static final int textView2=0x7f080009;
+        public static final int textView3=0x7f08000a;
+        public static final int textView4=0x7f08000b;
+        public static final int username=0x7f08000d;
     }
     public static final class layout {
-        public static final int activity_collection_demo=0x7f030000;
-        public static final int activity_login=0x7f030001;
-        public static final int activity_main=0x7f030002;
-        public static final int drawer_list_item=0x7f030003;
-        public static final int event=0x7f030004;
-        public static final int fragment_collection_object=0x7f030005;
-        public static final int fragment_planet=0x7f030006;
-        public static final int fragment_section_dummy=0x7f030007;
-        public static final int fragment_section_launchpad=0x7f030008;
-        public static final int login2=0x7f030009;
-        public static final int menu=0x7f03000a;
+        public static final int activity_check_list=0x7f030000;
+        public static final int activity_collection_demo=0x7f030001;
+        public static final int activity_event_first_step=0x7f030002;
+        public static final int activity_event_second_step=0x7f030003;
+        public static final int activity_login=0x7f030004;
+        public static final int activity_main=0x7f030005;
+        public static final int drawer_list_item=0x7f030006;
+        public static final int event=0x7f030007;
+        public static final int fragment_collection_object=0x7f030008;
+        public static final int fragment_planet=0x7f030009;
+        public static final int fragment_section_dummy=0x7f03000a;
+        public static final int fragment_section_launchpad=0x7f03000b;
+        public static final int login2=0x7f03000c;
+        public static final int menu=0x7f03000d;
+    }
+    public static final class menu {
+        public static final int check_list=0x7f070000;
+        public static final int event_first_step=0x7f070001;
+        public static final int event_second_step=0x7f070002;
     }
     public static final class string {
         public static final int Login=0x7f050006;
-        public static final int action_sign_in=0x7f05000e;
-        public static final int action_sign_in_short=0x7f05000f;
+        public static final int action_settings=0x7f05000d;
+        public static final int action_sign_in=0x7f05001c;
+        public static final int action_sign_in_short=0x7f05001d;
         public static final int action_websearch=0x7f050004;
         public static final int app_name=0x7f050000;
         public static final int app_not_available=0x7f050005;
@@ -76,17 +97,30 @@ public final class R {
         public static final int drawer_close=0x7f050003;
         public static final int drawer_open=0x7f050002;
         public static final int dummy_section_text=0x7f050001;
-        public static final int error_field_required=0x7f050013;
-        public static final int error_incorrect_password=0x7f050012;
-        public static final int error_invalid_email=0x7f050010;
-        public static final int error_invalid_password=0x7f050011;
+        public static final int error_field_required=0x7f050021;
+        public static final int error_incorrect_password=0x7f050020;
+        public static final int error_invalid_email=0x7f05001e;
+        public static final int error_invalid_password=0x7f05001f;
+        public static final int firstWizardDescription=0x7f050011;
+        public static final int firstWizardTitle=0x7f050010;
+        public static final int firstWizardTop=0x7f05000f;
+        public static final int firstWizardWhere=0x7f050012;
+        public static final int hello_world=0x7f05000c;
         public static final int logo=0x7f050008;
+        public static final int next_wizard=0x7f05000e;
         public static final int password=0x7f05000a;
         /**  Strings related to login 
          */
-        public static final int prompt_email=0x7f05000c;
-        public static final int prompt_password=0x7f05000d;
-        public static final int title_activity_login=0x7f05000b;
+        public static final int prompt_email=0x7f05001a;
+        public static final int prompt_password=0x7f05001b;
+        public static final int secondWizardDate=0x7f050016;
+        public static final int secondWizardTime=0x7f050015;
+        public static final int thirdWizardCheckList=0x7f050018;
+        public static final int title_activity_check_list=0x7f050017;
+        public static final int title_activity_event_first_step=0x7f05000b;
+        public static final int title_activity_event_second_step=0x7f050013;
+        public static final int title_activity_login=0x7f050019;
         public static final int username=0x7f050009;
+        public static final int when=0x7f050014;
     }
 }
