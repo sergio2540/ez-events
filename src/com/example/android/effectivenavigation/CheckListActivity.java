c44c196a04f6a6ca7a0f8aa6c477390b600df099
Bruno Braga <bruno.mh.braga@gmail.com>
2014-11-17 21:44:42 +0000

    Check list add to be tested

--- src/com/example/android/effectivenavigation/CheckListActivity.java
+++ src/com/example/android/effectivenavigation/CheckListActivity.java
@@ -1,32 +1,63 @@
 package com.example.android.effectivenavigation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class CheckListActivity extends Activity {
+
+	List<String> checkList = new ArrayList<String>();
+	String mText;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_check_list);
-		
+
 		Button mCheckListButton = (Button) findViewById(R.id.check_list_next);
 
 		mCheckListButton.setOnClickListener(new OnClickListener() {
-		    @Override
-		    public void onClick(View view) {
-			Intent intent = new Intent(view.getContext(), GuestsActivity.class);
-			//attemptLogin();
-			startActivity(intent);
+			@Override
+			public void onClick(View view) {
+				Intent intent = new Intent(view.getContext(), GuestsActivity.class);
+				//attemptLogin();
+				startActivity(intent);
 
-		    }
+			}
 		});
+
+		ListView checkListView =  (ListView)findViewById(R.id.checkList);
+		ArrayAdapter<String> checkListAdapter = new ArrayAdapter<String>(this,R.layout.check_list_view, checkList);
+		checkListAdapter.notifyDataSetChanged();
+
+		checkListView.setOnItemLongClickListener(new OnItemLongClickListener() {
+
+
+			@Override
+			public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
+					int arg2, long arg3) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+		});
+
+		checkListView.setAdapter(checkListAdapter);
 	}
 
 	@Override
@@ -45,6 +76,44 @@ public class CheckListActivity extends Activity {
 		if (id == R.id.action_settings) {
 			return true;
 		}
+		else if (id == R.id.action_add_todo)
+		{
+
+			showDialog();
+			checkList.add(mText);
+			
+			
+
+		}
 		return super.onOptionsItemSelected(item);
+	}
+
+
+
+	private void showDialog() {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle("Title");
+
+		// Set up the input
+		final EditText input = new EditText(this);
+		// Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+		input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+		builder.setView(input);
+
+		// Set up the buttons
+		builder.setPositiveButton("OK", new DialogInterface.OnClickListener() { 
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				mText = input.getText().toString();
+			}
+		});
+		builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.cancel();
+			}
+		});
+
+		builder.show();
 	}
 }
