f6067ded024dc89d75f300dfad06e6576bd52997
Sérgio Alves <sergio2540@gmail.com>
2014-11-17 22:22:27 +0000

    new todo working

--- src/com/example/android/effectivenavigation/CheckListActivity.java
+++ src/com/example/android/effectivenavigation/CheckListActivity.java
@@ -22,98 +22,97 @@ import android.widget.ListView;
 
 public class CheckListActivity extends Activity {
 
-	List<String> checkList = new ArrayList<String>();
-	String mText;
+    List<String> checkList = new ArrayList<String>();
+    String mText;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_check_list);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_check_list);
 
-		Button mCheckListButton = (Button) findViewById(R.id.check_list_next);
+	Button mCheckListButton = (Button) findViewById(R.id.check_list_next);
 
-		mCheckListButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				Intent intent = new Intent(view.getContext(), GuestsActivity.class);
-				//attemptLogin();
-				startActivity(intent);
+	mCheckListButton.setOnClickListener(new OnClickListener() {
+	    @Override
+	    public void onClick(View view) {
+		Intent intent = new Intent(view.getContext(), GuestsActivity.class);
+		//attemptLogin();
+		startActivity(intent);
 
-			}
-		});
+	    }
+	});
 
-		ListView checkListView =  (ListView)findViewById(R.id.checkList);
-		ArrayAdapter<String> checkListAdapter = new ArrayAdapter<String>(this,R.layout.check_list_view, checkList);
-		checkListAdapter.notifyDataSetChanged();
+	ListView checkListView =  (ListView)findViewById(R.id.checkList);
+	ArrayAdapter<String> checkListAdapter = new ArrayAdapter<String>(this,R.layout.check_list_view, checkList);
+	checkListAdapter.notifyDataSetChanged();
 
-		checkListView.setOnItemLongClickListener(new OnItemLongClickListener() {
+	checkListView.setOnItemLongClickListener(new OnItemLongClickListener() {
 
 
-			@Override
-			public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
-					int arg2, long arg3) {
-				// TODO Auto-generated method stub
-				return false;
-			}
-		});
+	    @Override
+	    public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
+		    int arg2, long arg3) {
+		// TODO Auto-generated method stub
+		return false;
+	    }
+	});
 
-		checkListView.setAdapter(checkListAdapter);
+	checkListView.setAdapter(checkListAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+	// Inflate the menu; this adds items to the action bar if it is present.
+	getMenuInflater().inflate(R.menu.check_list, menu);
+	return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+	// Handle action bar item clicks here. The action bar will
+	// automatically handle clicks on the Home/Up button, so long
+	// as you specify a parent activity in AndroidManifest.xml.
+	int id = item.getItemId();
+	if (id == R.id.action_settings) {
+	    return true;
 	}
+	else if (id == R.id.action_add_todo)
+	{
+	    showDialog();
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.check_list, menu);
-		return true;
 	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		else if (id == R.id.action_add_todo)
-		{
-
-			showDialog();
-			checkList.add(mText);
-			
-			
-
-		}
-		return super.onOptionsItemSelected(item);
-	}
+	return super.onOptionsItemSelected(item);
+    }
 
 
 
-	private void showDialog() {
-		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		builder.setTitle("Title");
+    private void showDialog() {
+	AlertDialog.Builder builder = new AlertDialog.Builder(this);
+	builder.setTitle("Add TODO");
 
-		// Set up the input
-		final EditText input = new EditText(this);
-		// Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
-		input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
-		builder.setView(input);
+	// Set up the input
+	final EditText input = new EditText(this);
+	// Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+	input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_AUTO_CORRECT | InputType.TYPE_TEXT_FLAG_MULTI_LINE );
+	builder.setView(input);
 
-		// Set up the buttons
-		builder.setPositiveButton("OK", new DialogInterface.OnClickListener() { 
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				mText = input.getText().toString();
-			}
-		});
-		builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				dialog.cancel();
-			}
-		});
+	// Set up the buttons
+	builder.setPositiveButton("Add", new DialogInterface.OnClickListener() { 
+	    @Override
+	    public void onClick(DialogInterface dialog, int which) {
 
-		builder.show();
-	}
+		checkList.add(input.getText().toString());
+		
+
+	    }
+	});
+	builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+	    @Override
+	    public void onClick(DialogInterface dialog, int which) {
+		dialog.cancel();
+	    }
+	});
+
+	builder.show();
+    }
 }
