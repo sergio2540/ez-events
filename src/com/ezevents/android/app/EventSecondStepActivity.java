cf6acceaf53efbd3adb080801de9052f11032c66
Sérgio Alves <sergio2540@gmail.com>
2014-11-19 23:33:29 +0000

    add map

--- src/com/ezevents/android/app/EventSecondStepActivity.java
+++ src/com/ezevents/android/app/EventSecondStepActivity.java
@@ -5,10 +5,25 @@ import java.util.Calendar;
 import java.util.Locale;
 
 
+
+
+
+
+
+
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -22,112 +37,113 @@ import android.widget.TimePicker;
 
 public class EventSecondStepActivity extends Activity {
 
-	Calendar myCalendar = Calendar.getInstance();
-	EditText editDate;
-	EditText editTime;
+    Calendar myCalendar = Calendar.getInstance();
+    EditText editDate;
+    EditText editTime;
 
+
+
+    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+
+
+	@Override
+	public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+	    // TODO Auto-generated method stub
+	    myCalendar.set(Calendar.YEAR, year);
+	    myCalendar.set(Calendar.MONTH, monthOfYear);
+	    myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+	    updateLabel();
+	}
+    };
+
+
+    private void updateLabel() {
+
+	String myFormat = "MM/dd/yy"; //In which you need put here
+	SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+	editDate.setText(sdf.format(myCalendar.getTime()));
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_event_second_step);
 	
 
-	DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
-		
-		
-
-		@Override
-		public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-			// TODO Auto-generated method stub
-			myCalendar.set(Calendar.YEAR, year);
-			myCalendar.set(Calendar.MONTH, monthOfYear);
-			myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-			updateLabel();
-		}
-	};
+	editDate = (EditText) findViewById(R.id.date); 
+	editTime = (EditText) findViewById(R.id.time); 
 
 
-	private void updateLabel() {
+	Button mSecondNextButton = (Button) findViewById(R.id.secondNextButton);
 
-		String myFormat = "MM/dd/yy"; //In which you need put here
-		SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+	mSecondNextButton.setOnClickListener(new OnClickListener() {
+	    @Override
+	    public void onClick(View view) {
+		Intent intent = new Intent(view.getContext(), MapsActivity.class);
+		//attemptLogin();
+		startActivity(intent);
 
-		editDate.setText(sdf.format(myCalendar.getTime()));
+	    }
+	});
+
+	editDate.setOnClickListener(new OnClickListener() {
+
+	    @Override
+	    public void onClick(View v) {
+		// TODO Auto-generated method stub
+		new DatePickerDialog(v.getContext(), date, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+			myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+
+	    }
+	});
+
+	editTime.setOnClickListener(new OnClickListener() {
+
+	    @Override
+	    public void onClick(View v) {
+		// TODO Auto-generated method stub
+		Calendar mcurrentTime = Calendar.getInstance();
+		int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+		int minute = mcurrentTime.get(Calendar.MINUTE);
+		TimePickerDialog mTimePicker;
+		mTimePicker = new TimePickerDialog(v.getContext(), new TimePickerDialog.OnTimeSetListener() {
+		    @Override
+		    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+			editTime.setText( selectedHour + ":" + selectedMinute);
+		    }
+		}, hour, minute, true);//Yes 24 hour time
+		mTimePicker.setTitle("Select Time");
+		mTimePicker.show();
+
+	    }
+	});
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+	// Inflate the menu; this adds items to the action bar if it is present.
+	getMenuInflater().inflate(R.menu.event_second_step, menu);
+	return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+	// Handle action bar item clicks here. The action bar will
+	// automatically handle clicks on the Home/Up button, so long
+	// as you specify a parent activity in AndroidManifest.xml.
+	int id = item.getItemId();
+	if (id == R.id.action_settings) {
+	    return true;
 	}
+	return super.onOptionsItemSelected(item);
+    }
+    public void displayDateSelection(View v){
 
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_event_second_step);
-		
-		editDate = (EditText) findViewById(R.id.date); 
-		editTime = (EditText) findViewById(R.id.time); 
-
-
-		Button mSecondNextButton = (Button) findViewById(R.id.secondNextButton);
-
-		mSecondNextButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				Intent intent = new Intent(view.getContext(), CheckListActivity.class);
-				//attemptLogin();
-				startActivity(intent);
-
-			}
-		});
-		
-		editDate.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				new DatePickerDialog(v.getContext(), date, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
-						myCalendar.get(Calendar.DAY_OF_MONTH)).show();
-				
-			}
-		});
-		
-		editTime.setOnClickListener(new OnClickListener() {
-
-	        @Override
-	        public void onClick(View v) {
-	            // TODO Auto-generated method stub
-	            Calendar mcurrentTime = Calendar.getInstance();
-	            int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
-	            int minute = mcurrentTime.get(Calendar.MINUTE);
-	            TimePickerDialog mTimePicker;
-	            mTimePicker = new TimePickerDialog(v.getContext(), new TimePickerDialog.OnTimeSetListener() {
-	                @Override
-	                public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
-	                    editTime.setText( selectedHour + ":" + selectedMinute);
-	                }
-	            }, hour, minute, true);//Yes 24 hour time
-	            mTimePicker.setTitle("Select Time");
-	            mTimePicker.show();
-
-	        }
-	    });
-		
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.event_second_step, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-	public void displayDateSelection(View v){
-
-
-	}
+    }
 
 }
